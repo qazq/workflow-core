94d021d8cae32d644bb91e5aaae0bfb8589eec00
Daniel Gerlag <daniel@gerlag.co.za>
2017-03-05 16:31:36 -0800

    tests

---
+++ test/WorkflowCore.IntegrationTests/Scenarios/BaseScenario.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+
+namespace WorkflowCore.IntegrationTests.Scenarios
+{
+    public abstract class BaseScenario : IDisposable
+    {
+        protected IWorkflowHost Host;
+        protected IPersistenceProvider PersistenceProvider;
+
+        public BaseScenario()
+        {            
+            IServiceCollection services = new ServiceCollection();
+            services.AddLogging();
+            services.AddWorkflow();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.AddConsole(LogLevel.Debug);
+            
+            PersistenceProvider = serviceProvider.GetService<IPersistenceProvider>();
+            Host = serviceProvider.GetService<IWorkflowHost>();
+            RegisterWorkflows();
+            Host.Start();
+        }
+
+        protected abstract void RegisterWorkflows();
+
+        public void Dispose()
+        {
+            Host.Stop();
+        }
+    }
+}
--- test/WorkflowCore.IntegrationTests/Scenarios/BasicWorkflow.cs
+++ test/WorkflowCore.IntegrationTests/Scenarios/BasicWorkflow.cs
@@ -1,7 +1,4 @@
-﻿using Machine.Fakes;
-using Machine.Fakes.Adapters.Moq;
-using Machine.Specifications;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -10,27 +7,29 @@ using System.Threading.Tasks;
 using WorkflowCore.Interface;
 using WorkflowCore.Models;
 using WorkflowCore.Services;
+using Xunit;
+using FluentAssertions;
 
 namespace WorkflowCore.IntegrationTests.Scenarios
 {
-    [Subject(typeof(WorkflowHost))]
-    public class BasicWorkflowTest : WithFakes<MoqFakeEngine>
+    
+    public class BasicWorkflowTest : BaseScenario
     {
-
-        static int Step1Ticker = 0;
-        static int Step2Ticker = 0;
-
+        
         public class Step1 : StepBody
         {            
             public override ExecutionResult Run(IStepExecutionContext context)
             {
-                Step1Ticker++;
+                BasicWorkflow.Step1Ticker++;
                 return ExecutionResult.Next();
             }
         }        
 
         class BasicWorkflow : IWorkflow
         {
+            public static int Step1Ticker = 0;
+            public static int Step2Ticker = 0;
+
             public string Id { get { return "BasicWorkflow"; } }
             public int Version { get { return 1; } }
             public void Build(IWorkflowBuilder<Object> builder)
@@ -45,56 +44,31 @@ namespace WorkflowCore.IntegrationTests.Scenarios
                         
             }
         }
-                        
-        static IWorkflowHost Host;
-        static string WorkflowId;
-        static IPersistenceProvider PersistenceProvider;
-        static WorkflowInstance Instance;
 
-
-        Establish context = () =>
+        protected override void RegisterWorkflows()
         {
-            //setup dependency injection
-            IServiceCollection services = new ServiceCollection();
-            services.AddLogging();
-            services.AddWorkflow();
-            
-            var serviceProvider = services.BuildServiceProvider();
+            Host.RegisterWorkflow<BasicWorkflow>();
+        }
 
-            //config logging
-            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-            loggerFactory.AddConsole(LogLevel.Debug);
-
-            var registry = serviceProvider.GetService<IWorkflowRegistry>();            
-            registry.RegisterWorkflow(new BasicWorkflow());
-
-            PersistenceProvider = serviceProvider.GetService<IPersistenceProvider>();
-            Host = serviceProvider.GetService<IWorkflowHost>();
-            Host.Start();            
-        };
-
-        Because of = () =>
+        [Fact]
+        public void Scenario()
         {
-            WorkflowId = Host.StartWorkflow("BasicWorkflow").Result;
-            Instance = PersistenceProvider.GetWorkflowInstance(WorkflowId).Result;
+            //act
+            string workflowId = Host.StartWorkflow("BasicWorkflow").Result;
+            WorkflowInstance instance = PersistenceProvider.GetWorkflowInstance(workflowId).Result;
             int counter = 0;
-            while ((Instance.Status == WorkflowStatus.Runnable) && (counter < 60))
+            while ((instance.Status == WorkflowStatus.Runnable) && (counter < 60))
             {
                 System.Threading.Thread.Sleep(500);
                 counter++;
-                Instance = PersistenceProvider.GetWorkflowInstance(WorkflowId).Result;                
+                instance = PersistenceProvider.GetWorkflowInstance(workflowId).Result;
             }
-        };
 
-        It should_be_marked_as_complete = () => Instance.Status.ShouldEqual(WorkflowStatus.Complete);
-        It should_execute_step1_once = () => Step1Ticker.ShouldEqual(1);
-        It should_execute_step2_once = () => Step2Ticker.ShouldEqual(1);
-
-        Cleanup after = () =>
-        {
-            Host.Stop();
-        };
-
+            //assert            
+            instance.Status.ShouldBeEquivalentTo(WorkflowStatus.Complete);
+            BasicWorkflow.Step1Ticker.ShouldBeEquivalentTo(1);
+            BasicWorkflow.Step2Ticker.ShouldBeEquivalentTo(1);
+        }        
 
     }
 }
